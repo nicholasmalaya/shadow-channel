4feae37dacbddb09ec21415bffecaf157cf6ca46
Nicholas Malaya <nick@ices.utexas.edu>
2013-09-19 14:59:06 -0500

    [shadow channel]: adding notes

--- drivers/keefe.py
+++ drivers/keefe.py
@@ -6,6 +6,12 @@ import sys
 sys.path.append("../channel/")
 import channel 
 
+# try subprocess to 
+# redirect all the screen outputs in C  to a buffer
+#  which we can access in Python through channel.stdout
+#
+# http://docs.python.org/2/library/subprocess.html
+
 # will segfault, at present
 # channel.init(16,32,16,1.6,1.6,32,1,0.01,1,5,1069,0)
 channel.init(16,32,16,1.6,1.6,32,1,0.01,1,5,100,0)
